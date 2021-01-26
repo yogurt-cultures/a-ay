c071400fd0fb8cfdc6892ecb75c3d08a28b0625d
Fatih Erikli <fatiherikli@gmail.com>
2021-01-26 02:50:53 +0100

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,4 @@
 AlphaBetaGamma = {
-  'a': 'a',
   'A' : ['LATIN CAPITAL LETTER A', 'Unicode: U+0041, UTF-8', 41],
   'B' : ['LATIN CAPITAL LETTER B', 'Unicode: U+0042, UTF-8', 42],
   'C' : ['LATIN CAPITAL LETTER C', 'Unicode: U+0043, UTF-8', 43],
