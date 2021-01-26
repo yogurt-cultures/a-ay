dec537d6aeadc323ce77c1ea70d18adfca012b33
Fatih Erikli <fatiherikli@gmail.com>
2021-01-26 02:50:36 +0100

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,66 +1,7 @@
 AlphaBetaGamma = {
   'a': 'a',
-
-  
-  A
-  LATIN CAPITAL LETTER A
-  Unicode: U+0041, UTF-8: 41
-  
-
-  'ą' : 'ą',
-  'b' : 'b',
-  
-  B
-  LATIN CAPITAL LETTER B
-  Unicode: U+0042, UTF-8: 42
-
-  
-  'c' : 'c',
-
-  C
-  LATIN CAPITAL LETTER C
-  Unicode: U+0043, UTF-8: 43
-  
-  
-  'ć' : 'ć',
-  'd' : 'd',
-  
-  D
-  LATIN CAPITAL LETTER D
-  Unicode: U+0044, UTF-8: 44
-  
-  
-  'e' : 'e',
-  'ę' : 'ę',
-  'f' : 'f',
-  'g' : 'g',
-  'h' : 'o',
-  'ı' : 'i',
-  'i' : 'i',
-  'j' : 'j',
-  'k' : 'k',
-  'l' : 'l',
-  'ł' : 'ł',
-  'm' : 'm',
-  'n' : 'n',
-  'ń' : 'ń',
-  'o' : 'o',
-  'ó' : 'ó',
-  'ö' : 'ö',
-  'p' : 'p',
-  'r' : 'r',
-  's' : 's',
-  'ś' : 'ś',
-  'ş' : 'ş',
-  't' : 't',
-  'u' : 'u',
-  'ü' : 'ü',
-  'v' : 'v',
-  'y' : 'y',
-  'z' : 'z',
-  'ź' : 'ź',
+  'A' : ['LATIN CAPITAL LETTER A', 'Unicode: U+0041, UTF-8', 41],
+  'B' : ['LATIN CAPITAL LETTER B', 'Unicode: U+0042, UTF-8', 42],
+  'C' : ['LATIN CAPITAL LETTER C', 'Unicode: U+0043, UTF-8', 43],
+  'D' : ['LATIN CAPITAL LETTER D', 'Unicode: U+0044, UTF-8', 44]
 }
-
-X = AlphaBetaGamma
-Y = 0
-Z = 'z'
